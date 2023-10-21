7ffca8a2855813c588e21b71521dee223bd1ac6f
Diego Alexander Rivera Acosta <diegoarivera35@gmail.com>
2023-10-21 02:08:02 -0400

    new commit ot fix issues og j3

---
+++ assigment2/Controllers/J3Controller.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace assigment2.Controllers
+{
+    public class J3Controller : ApiController
+    {
+
+
+        /// <summary>
+        /// link to J3 ExactlyElectrical : chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://cemc.math.uwaterloo.ca/contests/computing/past_ccc_contests/2017/stage%201/juniorEF.pdf
+        /// a program to calculate if you can get from point a to point b in a Cartesian plane where there are intersections every 1 unit in x and in Y, the amount of moves
+        /// you can make trough the intersections depends on the number of batteries you have, you will spend them by passing intersections, each intersection is one less
+        /// battery points you have
+        /// </summary>
+        /// <param name="a"></param> first  X coordinate
+        /// <param name="b"></param> first y coordinate
+        /// <param name="c"></param> second x coordinate
+        /// <param name="d"></param> second y coordinate
+        /// <param name="t"></param> number of batteries cells
+        /// <returns>answer</returns> --> the answer is either y (yes) or n (no)
+        // GET api/j3/a/b/c/d/t
+        [Route("api/j3/{a}/{b}/{c}/{d}/{t}")]
+        public string Get(int a, int b, int c, int d, int t)
+        {
+            //add the coordinates to get the amount of intersections
+            int avenue = a + b;
+            int street = c + d;
+            int battery = t;
+
+            // Use this method to always get a positive number (intersections to destination) so we can know if the battery will be enought to get to the destination
+            int numberOfItersections = Math.Abs(avenue - street);
+
+            string answer;
+
+            // validate the conditions to know if the battery is enought, if its bigger than number of intersections but also
+            // the intersection must be even and the number of battery must be even the result is yes. 
+            //the other possibility is that the battery is bigger than the number of intersections and thenumber of battery and the number of intersections is odd
+            // sny other possibilities the answer is NO ("n")
+            if (battery % 2 == 1 && numberOfItersections % 2 == 1 && battery > numberOfItersections)
+            {
+                answer = "y";
+            }
+            else if (battery % 2 == 0 && numberOfItersections % 2 == 0 && battery > numberOfItersections)
+            {
+                answer = "y";
+            }
+            else
+            {
+                answer = "n";
+            }
+
+            return answer;
+        }
+
+
+
+    }
+}
---
+++ assigment2/Controllers/J4Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace assigment2.Controllers
+{
+    public class J4Controller : ApiController
+    {
+        //I tried to do this excersice j4 ArrivalTime
+        //link: chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://cemc.math.uwaterloo.ca/contests/computing/past_ccc_contests/2016/stage%201/juniorEn.pdf
+        //but i was getting an error when entering 05:00, it says that : is a security issue so i coudnt finish it
+
+        // GET api/j4/factor1/
+        [Route("api/j4/{factor1}")]
+        public string Get(string factor1)
+        {
+            string timeString = factor1;
+            string[] timeParts = timeString.Split(':');
+
+
+
+            //if is false return default value "sad"
+            return "hola";
+        }
+
+
+    }
+}
